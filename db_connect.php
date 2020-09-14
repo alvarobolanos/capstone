467f69d40413f6fbe3a2321a36629dfdb39cb7e0
Alvaro Bolanos <alvarobolanos@gmail.com>
2020-09-13 23:07:08 -0400

    Corrected db connection and added an index.js

--- db_connect.php
+++ db_connect.php
@@ -6,6 +6,6 @@ $usr = 'alvaro';
 $pass = 'password';
 $db = 'capstone';
 
-$mysqli = @mysqli_connect($host, $usr, $pass, $db, $port) OR die(mysqli_connect_error($dbc));
+$mysqli = @mysqli_connect($host, $usr, $pass, $db, $port) OR die(mysqli_connect_error($db));
 
 ?>
---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function createEventListeners() {
+	
+}
+
+
+if (window.addEventListener) {
+	window.addEventListener("load", createEventListeners, false);
+} else if (window.attachEvent) {
+	window.attachEvent("onload", createEventListeners);
+}
