866a17da1e74003c7c744db2053a753271280e83
Alvaro Bolanos <alvarobolanos@gmail.com>
2020-09-14 23:00:44 -0400

    Modified structure to qanda vs 2 different tables

--- database.sql
+++ database.sql
@@ -17,7 +17,8 @@ DROP TABLE IF EXISTS `games`;
 CREATE TABLE `games` (
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`title` VARCHAR(250) NOT NULL UNIQUE, 
-    `description` TEXT NOT NULL,
+	`description` TEXT NOT NULL,
+	`image_url` VARCHAR(2083),
 	`created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`last_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -39,74 +40,37 @@ CREATE TABLE `players_scores` (
 	FOREIGN KEY (`score_id`) REFERENCES scores(`id`)
 );
 
-DROP TABLE IF EXISTS `questions`;
-CREATE TABLE `questions` (
+DROP TABLE IF EXISTS `qanda`;
+CREATE TABLE `qanda` (
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`question_text` TEXT NOT NULL,
-	`created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `question` TEXT NOT NULL,
+    `answer_1` TINYTEXT NOT NULL,
+    `answer_2` TINYTEXT NOT NULL,
+    `answer_3` TINYTEXT NOT NULL,
+    `answer_4` TINYTEXT NOT NULL,
+    `correct_answer` TINYTEXT NOT NULL,
+    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`last_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`game_id` INT,
+    `game_id` INT,
 	FOREIGN KEY (`game_id`) REFERENCES games(`id`)
 );
 
-DROP TABLE IF EXISTS `answers`;
-CREATE TABLE `answers`(
-	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`answer_text` TINYTEXT NOT NULL,
-	`meta` ENUM ('correct', 'incorrect') NOT NULL, 
-	`created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`last_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`question_id` INT,
-	FOREIGN KEY (`question_id`) REFERENCES questions(`id`)
-);
-
 INSERT 
-INTO capstone.games (title, description)
-VALUES ('World Geography', 'A trivia game about world geography in which you will test your knowledge about places, bodies of water and other interesting facts that everyone should know. If you do not know these, then you should have stayed in school kido.');
+INTO capstone.games (title, description, image_url)
+VALUES ('World Geography', 'A trivia game about world geography in which you will test your knowledge about places, bodies of water and other interesting facts that everyone should know. If you do not know these, then you should have stayed in school kido.','https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/OrteliusWorldMap.jpeg/2560px-OrteliusWorldMap.jpeg');
 
 SELECT * FROM games;
 
-INSERT 
-INTO capstone.questions (question_text, game_id)
-VALUES ('What is the largest Island in the Caribbean?', 1),
-('Where is the tallest waterfall located?', 1),
-('What is the tallest peak in Europe?', 1),
-('In what country could you find Tungurahua volcano?', 1),
-('What is the biggest metropolitan area (by population) in the USA?', 1),
-('What country has the highest bird diversity?', 1),
-('What is the country with the largest forest area?', 1);
+INSERT
+INTO capstone.qanda (question, answer_1, answer_2, answer_3, answer_4, correct_answer, game_id)
+VALUES ('What is the largest Island in the Caribbean?', 'Puerto Rico', 'La Hispaniola', 'Cuba', 'Dominica', 'Cuba', 1), 
+('Where is the tallest waterfall located?','Venezuela','USA','Congo','Denmark','Denmark',1),
+('What is the tallest peak in Europe?','Mt. Elbrus','Mt. Blanc','Mt. Olympus','Mt. Edna','Mt. Elbrus',1),
+('In what country could you find Tungurahua volcano?','Costa Rica','Ecuador','Mexico','Chile','Ecuador',1),
+('What is the biggest metropolitan area (by population) in the USA?','Boston-Cambridge-Newton','Dallas-Fort Worth','Miami-Ft. Lauderdale-West Palm Beach','New York-Newark-Jersey City','New York-Newark-Jersey City',1),
+('What country has the highest bird diversity?','USA','India','Colombia','Indonesia','Colombia',1),
+('What is the country with the largest forest area?','Russian Federation','Brazil','Canada','China','Russian Federation',1);
 
-SELECT * FROM questions;
+SELECT * FROM qanda;
 
-INSERT 
-INTO capstone.answers (answer_text, meta, question_id)
-Values ('Puerto Rico','incorrect',1),
-('La Hispaniola','incorrect',1),
-('Cuba','correct',1),
-('Dominica','incorrect',1),
-('Venezuela','incorrect',2),
-('USA','incorrect',2),
-('Congo','incorrect',2),
-('Denmark','correct',2),
-('Mt. Elbrus','correct',3),
-('Mt. Blanc','incorrect',3),
-('Mt. Olympus','incorrect',3),
-('Mt. Edna','incorrect',3),
-('Costa Rica','incorrect',4),
-('Ecuador','correct',4),
-('Mexico','incorrect',4),
-('Chile','incorrect',4),
-('Boston-Cambridge-Newton','incorrect',5),
-('Dallas-Fort Worth','incorrect',5),
-('Miami-Ft. Lauderdale-West Palm Beach','incorrect',5),
-('New York-Newark-Jersey City','correct',5),
-('USA','incorrect',6),
-('India','incorrect',6),
-('Colombia','correct',6),
-('Indonesia','incorrect',6),
-('Russian Federation','correct',7),
-('Brazil','incorrect',7),
-('Canada','incorrect',7),
-('China','incorrect',7);
-
-SELECT * FROM answers WHERE question_id=1 AND meta='correct';
+SELECT question, correct_answer FROM capstone.qanda WHERE game_id=1;
